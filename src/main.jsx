a4a715a2a56fe7c246ba9d245532ffc49d966274
Marcos Alcantara <malcantara-2021332@kinal.edu.gt>
2023-05-06 19:48:53 -0600

    Agregando Card Hoteles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Login } from './Login'
+import HotelCard from './card/HotelCard';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
+    <Login/>
   </React.StrictMode>,
 )
