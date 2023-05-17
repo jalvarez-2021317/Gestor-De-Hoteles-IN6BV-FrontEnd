62ac75c4c3c5815842ac49c68a45a01f710e6ff9
jalvarez-2021317 <106627906+jalvarez-2021317@users.noreply.github.com>
2023-05-16 19:31:52 -0600

    Reservacion Test

--- src/api/ReservacionApi.js
+++ src/api/ReservacionApi.js
@@ -5,47 +5,54 @@ const BASE_URL = `http://localhost:8080/api`;
 
 // Función para realizar una reserva
 export const createReservation = async (usuario, habitacion, cantidad, hotel) => {
-  try {
-    const response = await axios.post(`${BASE_URL}/reservaciones/agregar`, {
-      usuario,
-      habitacion,
-      cantidad,
-      hotel,
-    });
+    try {
+        // Validar los datos de entrada
+        if (!usuario || !habitacion || !cantidad || !hotel) {
+            throw new Error("Por favor, proporciona todos los datos necesarios para realizar la reserva.");
+        }
 
-    if (response.status === 201) {
-      Swal.fire({
-        icon: "success",
-        title: "¡Reserva realizada!",
-        text: "¡La reserva se ha realizado correctamente!",
-        confirmButtonText: "Ok",
-      });
-      return response.data; // Puedes devolver los datos de la reserva si los necesitas
-    } else {
-      throw new Error();
+        const response = await axios.post(`${BASE_URL}/reservaciones/agregar`, {
+            usuario,
+            habitacion,
+            cantidad,
+            hotel,
+        });
+
+        if (response.status === 201) {
+            Swal.fire({
+                icon: "success",
+                title: "¡Reserva realizada!",
+                text: "¡La reserva se ha realizado correctamente!",
+                confirmButtonText: "Ok",
+            });
+            return response.data; // Puedes devolver los datos de la reserva si los necesitas
+        } else {
+            throw new Error("Error al realizar la reserva. Por favor, intenta nuevamente.");
+        }
+    } catch (error) {
+        // Mostrar mensaje de error específico al usuario
+        Swal.fire({
+            icon: "error",
+            title: "Error al realizar la reserva",
+            text: error.message,
+        });
+        return null;
     }
-  } catch (error) {
-    Swal.fire({
-      icon: "error",
-      title: "Error al realizar la reserva",
-      text: "Ha ocurrido un error al realizar la reserva. Por favor, intenta nuevamente.",
-    });
-    return null;
-  }
 };
 
 // Función para obtener todas las reservaciones
 export const getReservations = async () => {
     try {
-      const response = await axios.get(`${BASE_URL}/reservaciones/`);
-      return response.data.reservations || []; // Devuelve un array vacío si no hay reservaciones
+        const response = await axios.get(`${BASE_URL}/reservaciones/`);
+        console.log(response.data);
+        return response.data || []; // Devuelve un array vacío si no hay reservaciones
     } catch (error) {
-      Swal.fire({
-        icon: "error",
-        title: "Error al obtener las reservaciones",
-        text: "Ha ocurrido un error al obtener las reservaciones. Por favor, intenta nuevamente.",
-      });
-      return [];
+        // Mostrar mensaje de error al usuario
+        Swal.fire({
+            icon: "error",
+            title: "Error al obtener las reservaciones",
+            text: "Ha ocurrido un error al obtener las reservaciones. Por favor, intenta nuevamente.",
+        });
+        return [];
     }
-  };
-  
+};
--- src/components/Reservaciones.jsx
+++ src/components/Reservaciones.jsx
@@ -26,16 +26,23 @@ const Reservations = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = await createReservation(newReservation.usuario, newReservation.habitacion, newReservation.cantidad, newReservation.hotel);
+    const data = await createReservation(
+      newReservation.usuario,
+      newReservation.habitacion,
+      newReservation.cantidad,
+      newReservation.hotel
+    );
     if (data) {
       // Reserva exitosa, actualizar las reservaciones
-      fetchReservations();
+      setReservations([...reservations, data]);
       setNewReservation({
         usuario: "",
         habitacion: "",
         cantidad: 0,
         hotel: "",
       });
+    } else {
+      console.log("Error al crear la reservación");
     }
   };
 
@@ -43,21 +50,66 @@ const Reservations = () => {
     <div>
       <h2>Reservaciones</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="usuario"
-        value={newReservation.usuario} onChange={handleInputChange} placeholder="Usuario" />
-        <input type="text" name="habitacion" value={newReservation.habitacion} onChange={handleInputChange} placeholder="Habitación" />
-        <input type="number" name="cantidad" value={newReservation.cantidad} onChange={handleInputChange} placeholder="Cantidad" />
-        <input type="text" name="hotel" value={newReservation.hotel} onChange={handleInputChange} placeholder="Hotel" />
-        <button type="submit">Reservar</button>
-        </form>
-        <ul>
-        {reservations.map((reservation) => (
-        <li key={reservation._id}>
-        Usuario: {reservation.usuario}, Habitación: {reservation.habitacion}, Cantidad: {reservation.cantidad}, Hotel: {reservation.hotel}
-        </li>
-        ))}
-        </ul>
-        </div>
-        );
-        }
-        export default Reservations;
+      <input
+                    type="text"
+                    name="usuario"
+                    value={newReservation.usuario}
+                    onChange={handleInputChange}
+                    placeholder="Usuario"
+                />
+                <input
+                    type="text"
+                    name="habitacion"
+                    value={newReservation.habitacion}
+                    onChange={handleInputChange}
+                    placeholder="Habitación"
+                />
+                <input
+                    type="number"
+                    name="cantidad"
+                    value={newReservation.cantidad}
+                    onChange={handleInputChange}
+                    placeholder="Cantidad"
+                />
+                <input
+                    type="text"
+                    name="hotel"
+                    value={newReservation.hotel}
+                    onChange={handleInputChange}
+                    placeholder="Hotel"
+                />
+                <button type="submit">Reservar</button>
+      </form>
+      <div>
+        {Array.isArray(reservations) && reservations.length > 0 ? (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Usuario</th>
+                <th>Habitación</th>
+                <th>Cantidad</th>
+                <th>Hotel</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reservations.map((reservation) => (
+                <tr key={reservation._id}>
+                  <td>{reservation._id}</td>
+                  <td>{reservation.usuario}</td>
+                  <td>{reservation.habitacion}</td>
+                  <td>{reservation.cantidad}</td>
+                  <td>{reservation.hotel}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div>No hay reservaciones disponibles</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Reservations;
