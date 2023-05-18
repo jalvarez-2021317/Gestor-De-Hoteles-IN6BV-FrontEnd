5bb651155028eb12dfca9993cd1193c459c9e93c
Marcos Alcántara <malcantara-2021332@kinal.edu.gt>
2023-05-18 10:00:09 -0600

    Page Usuario

--- src/AdminHotel/BuscadorReseras.jsx
+++ src/AdminHotel/BuscadorReseras.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import jwt_decode from "jwt-decode";
+// import jwt_decode from "jwt-decode";
 
 import { createReservation, searchUserReservation, getReservations } from "../api/ReservacionApi";
 
